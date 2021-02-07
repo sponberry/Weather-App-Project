df4540a3e9ca16da2326f0e7a8d007cc246943ff
sponberry <Abi88392+sponberry@users.noreply.github.com>
2021-02-07 12:21:45 +0000

    Formatted weekly forecast as a list instead of separate div cols

--- src/script copy.js
+++ src/script copy.js
@@ -47,13 +47,7 @@ function showTemp(response) {
   displayedTemp.innerHTML = `${temp}°`;
 }
 
-function loopDays(response) {
-  let fiveDayForecast = document.querySelectorAll("li.days");
-
-  fiveDayForecast.forEach(showNextFiveDays, response);
-}
-
-function showNextFiveDays(dayElement, response) {
+function showNextFiveDays(response) {
   //tomorrow
   let tomorrowTemp = Math.round(response.data.list[1].main.temp);
   let tomorrowIcon = checkWeatherCode(response.data.list[1].weather[0].id);
